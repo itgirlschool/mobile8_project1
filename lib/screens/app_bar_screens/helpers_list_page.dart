8e97c56b1380c0d9fcd1b1f4538e1940bc27b661
Zoya Belova (macbook) <alirasirin@gmail.com>
2023-04-24 23:02:17 +0300

    Страница поиска помощников, профиль из объекта User

--- lib/screens/app_bar_screens/helpers_list_page.dart
+++ lib/screens/app_bar_screens/helpers_list_page.dart
@@ -1,43 +1,257 @@
 import 'package:flutter/material.dart';
+import 'package:mobile8_project1/screens/app_bar_screens/profile_user_page.dart';
 import 'package:mobile8_project1/screens/app_bar_screens/questions_page.dart';
+import '../../classes.dart';
+import '../../data/json_fetchers.dart';
 
-class HelpersListPage extends StatelessWidget {
-  const HelpersListPage({super.key});
+class HelpersListPage extends StatefulWidget {
+  const HelpersListPage({Key? key}) : super(key: key);
+
+  @override
+  State<HelpersListPage> createState() => _HelpersListPageState();
+}
+
+class _HelpersListPageState extends State<HelpersListPage> {
+  Future<List<User>>? userList;
+  String searchName = '';
+  bool searchDiploma = false;
+  final nameController = TextEditingController();
+  final ratingController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    nameController.dispose();
+    ratingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    userList = fetchSearchUserList();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      //backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Помощники'),
+      ),
       body: Container(
-        padding: const EdgeInsets.all(30),
+        padding: const EdgeInsets.all(20),
         child: ListView(
           children: [
-            const Text(
-              'Экран со списком помощников',
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 300,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => const ThemeQuestionPage()),
-                );
-              },
-              child: const Text(
-                  'Задать вопрос -  кнопка для теста для перехода а пункт меню в будущем'),
-            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => const ThemeQuestionPage()),
+                      );
+                    },
+                    child: const Text('Задать вопрос -  кнопка для теста для перехода а пункт меню в будущем'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => ProfileUserPage()),
+                      );
+                    },
+                    child: const Text('Профиль -  кнопка для теста для перехода а пункт меню в будущем'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _buildSearchHelper(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  if (userList != null) ...[
+                    FutureBuilder(
+                      future: userList,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData && snapshot.data != null) {
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              for (int i = 0; i < snapshot.data!.length; i++) ...[
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(builder: (context) => ProfileUserPage(user: snapshot.data![i])),
+                                    );
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(bottom: 10),
+                                    child: _buildUserRow(snapshot, i),
+                                  ),
+                                ),
+                              ],
+                            ],
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text('${snapshot.error}');
+                        }
+                        return const CircularProgressIndicator();
+                      },
+                    ),
+                  ],
 
-            // buildNameYouField(),
-            // biuldContactYouField(),
+                  // buildNameYouField(),
+                  // biuldContactYouField(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildSearchHelper() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Поиск',
+          style: TextStyle(fontSize: 20),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        TextField(
+          //controller: TextEditingController()..text = searchName,
+          controller: nameController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Имя',
+          ),
+          keyboardType: TextInputType.text,
+          onSubmitted: (String search) {
+            setState(() {
+              userList = fetchSearchUserList(nameQuery: search);
+              nameController.text = search;
+            });
+          },
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          //width: 150,
+          child: TextField(
+            //controller: TextEditingController()..text = searchName,
+            controller: ratingController,
+
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Рейтинг ❤️ от',
+            ),
+            keyboardType: TextInputType.number,
+            onSubmitted: (String search) {
+              setState(() {
+                int searchRating = search == '' ? 0 : int.parse(search);
+
+                userList = fetchSearchUserList(ratingQuery: searchRating);
+
+                ratingController.text = search;
+              });
+            },
+          ),
+        ),
+        CheckboxListTile(
+          value: searchDiploma,
+          contentPadding: EdgeInsets.only(left: 0),
+          controlAffinity: ListTileControlAffinity.leading,
+          title: const Text('Дипломированный психолог'),
+          onChanged: (bool? value) => setState(
+            () {
+              userList = fetchSearchUserList(diplomaQuery: value);
+              searchDiploma = value!;
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row _buildUserRow(AsyncSnapshot<List<User>> snapshot, int i) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: CircleAvatar(
+              radius: 40, // Image radius
+              backgroundImage: AssetImage(snapshot.data![i].photo)),
+        ),
+        Expanded(
+          flex: 3,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFFFFFFFF),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                //border: Border.all(color: const Color(0xFF71DEFF), width: 3),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      snapshot.data![i].name,
+                      style: const TextStyle(fontSize: 18), //!TODO изменять размер шрифта в зависимости от длины имени
+                    ),
+                    const SizedBox(
+                      height: 9,
+                    ),
+                    Row(
+                      children: [
+                        Text('❤️ ${snapshot.data![i].rating}'),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        if (snapshot.data![i].diploma == true) ...[
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(3),
+                              color: const Color(0xFF4AA9FC),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+                              child: Text(
+                                'психолог',
+                                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/screens/app_bar_screens/profile_user_page.dart
@@ -0,0 +1,218 @@
+import 'package:flutter/material.dart';
+
+import '../../classes.dart';
+
+class ProfileUserPage extends StatelessWidget {
+  User? user;
+
+  //ProfileUserPage({super.key, this.user});
+  ProfileUserPage({super.key, this.user}) {
+    user = user ?? User.testBasicUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PersonWidget(user: user!);
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  User? user;
+
+  FavoriteWidget({super.key, this.user});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState(user!);
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  User user;
+
+  _FavoriteWidgetState(this.user);
+
+  bool _isFavorited = false;
+
+  //_FavoriteWidgetState(user);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          child: IconButton(
+            onPressed: _toggleFavorite,
+            color: Colors.red[500],
+            icon: (_isFavorited ? Icon(Icons.favorite) : Icon(Icons.favorite_border)),
+          ),
+        ),
+        SizedBox(
+          width: 40,
+          child: Container(
+            //child: Text('$_favoriteCount'),
+            child: Text(user.rating.toString()),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _isFavorited = false;
+        user.rating -= 1;
+      } else {
+        _isFavorited = true;
+        user.rating += 1;
+      }
+    });
+  }
+}
+
+class PersonWidget extends StatelessWidget {
+  User? user;
+
+  PersonWidget({super.key, this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Профиль'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Container(
+        child: _buildMainColumn(),
+      ),
+    );
+  }
+
+  Widget _buildMainColumn() => ListView(
+        children: [
+          _builTopImage(),
+          Center(
+            child: Container(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(5),
+                    child: _buildRaiting(),
+                  ),
+                  Card(
+                    elevation: 5,
+                    margin: const EdgeInsets.all(5),
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      child: _buildAction(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(5),
+                    child: _buildDesc(),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      );
+
+  Widget _builTopImage() => Card(
+        margin: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+          top: 10,
+        ),
+        elevation: 5,
+        child: Image.asset(
+          user!.photo,
+          fit: BoxFit.cover,
+        ),
+      );
+
+  Widget _buildRaiting() => ListTile(
+        title: Text(
+          user!.name,
+          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+        ),
+        subtitle: Text(user!.city),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FavoriteWidget(user: user!),
+          ],
+        ),
+      );
+
+  Widget _buildAction() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildButton('${user!.numAnswers} ${_getAnswersTitle(user!.numAnswers)}', Icons.star, Colors.black),
+          if (user!.diploma) ...[
+            _buildButton('Диплом', Icons.topic, Colors.black),
+          ],
+          if (user!.experienceYears >0) ...[
+            _buildButton('Стаж ${user!.experienceYears} ${_getYearsTitle(user!.experienceYears)}', Icons.auto_graph, Colors.black),
+          ]
+        ],
+      );
+
+  Widget _buildButton(String label, IconData icon, Color color) => Column(
+        children: [
+          Icon(
+            icon,
+            color: Colors.black,
+          ),
+          Container(
+            child: Text(
+              label,
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+
+  Widget _buildDesc() => Text(
+        user!.aboutSelf,
+        softWrap: true,
+        style: TextStyle(fontSize: 16),
+      );
+}
+
+String _getAnswersTitle(int numAnswers) {
+  int lastDigit = numAnswers % 10;
+  int lastTwoDigits = numAnswers % 100;
+
+  if (lastDigit == 1 && lastTwoDigits != 11) {
+    return "совет";
+  } else if (lastDigit >= 2 && lastDigit <= 4 && (lastTwoDigits < 10 || lastTwoDigits > 20)) {
+    return "совета";
+  } else {
+    return "советов";
+  }
+}
+
+String _getYearsTitle(int numYears) {
+  int lastDigit = numYears % 10;
+  int lastTwoDigits = numYears % 100;
+
+  if (lastDigit == 1 && lastTwoDigits != 11) {
+    return "год";
+  } else if (lastDigit >= 2 && lastDigit <= 4 && (lastTwoDigits < 10 || lastTwoDigits > 20)) {
+    return "года";
+  } else {
+    return "лет";
+  }
+}
